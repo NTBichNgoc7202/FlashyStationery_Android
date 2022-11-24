83df5e391a9667e7f5859df44122eaa4806dd8d9
NTBichNgoc7202 <ngocntb20411c@st.uel.edu.vn>
2022-11-25 04:09:31 +0700

    Update UserProfileActivity, AndroidManifest.xml 25/11

--- app/src/main/java/com/race/flashystationery/UserProfileActivity.java
+++ app/src/main/java/com/race/flashystationery/UserProfileActivity.java
@@ -1,14 +1,41 @@
 package com.race.flashystationery;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+
+import com.race.flashystationery.databinding.ActivityUserProfileBinding;
 
 public class UserProfileActivity extends AppCompatActivity {
+
+    ActivityUserProfileBinding binding;
+//    ActionBar actionBar = getSupportActionBar();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_profile);
+//        setContentView(R.layout.activity_user_profile);
+
+        binding = ActivityUserProfileBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        customActionBar();
+
+
+    }
+
+    private void customActionBar() {
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.user_profile_menu,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
