5524bc275ed9352eedcbe67a9e42d0867e409324
NTBichNgoc7202 <ngocntb20411c@st.uel.edu.vn>
2022-11-26 22:47:15 +0700

    New drawable 26/11

--- app/src/main/java/com/race/flashystationery/UserProfileActivity.java
+++ app/src/main/java/com/race/flashystationery/UserProfileActivity.java
@@ -1,11 +1,13 @@
 package com.race.flashystationery;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.race.flashystationery.databinding.ActivityUserProfileBinding;
 
@@ -23,19 +25,25 @@ public class UserProfileActivity extends AppCompatActivity {
         binding = ActivityUserProfileBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        customActionBar();
-
-
-    }
-
-    private void customActionBar() {
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.user_profile_menu,menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.mn_Setting){
+            //Opening Setting Act
+
+        } else if (item.getItemId() == R.id.mn_Notify){
+            //Opening Notification Act
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
