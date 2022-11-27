651ce8ecf2a3d67255364aba48e895519050d3e7
VoThiNgocTrinh20411c <trinhvtn20411c@st.uel.edu.vn>
2022-11-27 18:19:40 +0700

    Login Page

--- app/src/main/java/com/race/flashystationery/LoginActivity.java
+++ app/src/main/java/com/race/flashystationery/LoginActivity.java
@@ -5,9 +5,10 @@ import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login);
     }
 }
