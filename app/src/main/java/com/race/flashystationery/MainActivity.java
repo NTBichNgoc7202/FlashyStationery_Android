f006d815d0d8df0188d02cf0e9629ff9e9b10a44
NTBichNgoc7202 <ngocntb20411c@st.uel.edu.vn>
2022-11-28 20:27:52 +0700

    Add bottom_nav_menu.xml 28/11 8:27

--- app/src/main/java/com/race/flashystationery/MainActivity.java
+++ app/src/main/java/com/race/flashystationery/MainActivity.java
@@ -1,6 +1,7 @@
 package com.race.flashystationery;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
     SharedPreferences preferences = null;
+    Fragment fragmentHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +24,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         preferences = getSharedPreferences("com.race.myAppName",MODE_PRIVATE);
-        //chạy thật xoá dòng dưới
-        preferences.edit().putBoolean("firstrun", true).commit();
 
 
+
+        //preferences.edit().putBoolean("firstrun", true).commit();
 
     }
 
