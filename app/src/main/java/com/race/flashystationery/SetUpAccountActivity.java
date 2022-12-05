71731c5f7c4aa33dab427ba3e00e6bb25bc1eb83
VoThiNgocTrinh20411c <trinhvtn20411c@st.uel.edu.vn>
2022-12-05 23:55:30 +0700

    update code registeractivity page + setupaccount page

--- app/src/main/java/com/race/flashystationery/SetUpAccountActivity.java
+++ app/src/main/java/com/race/flashystationery/SetUpAccountActivity.java
@@ -3,12 +3,33 @@ package com.race.flashystationery;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SetUpAccountActivity extends AppCompatActivity {
+
+    private TextView txtShowFullName, txtShowPassWordHint,txtShowEmail, txtShowPhoneNumber, txtShowGender, txtShowDOB, txtShowSocialMedia, txtShowAccountBank;
+    private ProgressBar progressBar;
+    private String fullName, eMail, doB, gender, phoneNumber;
+    private ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_up_account);
+
+        txtShowFullName = findViewById(R.id.txt_ShowFullName);
+        txtShowPassWordHint = findViewById(R.id.txt_ShowPasswordHint);
+        txtShowEmail = findViewById(R.id.txt_ShowEmail);
+        txtShowPhoneNumber = findViewById(R.id.txt_ShowPhoneNumber);
+        txtShowGender = findViewById(R.id.txt_ShowGender);
+        txtShowDOB = findViewById(R.id.txt_ShowDoB);
+        txtShowSocialMedia = findViewById(R.id.txt_ShowSocialMedia);
+        txtShowAccountBank = findViewById(R.id.txt_ShowAccountBank);
+        progressBar = findViewById(R.id.processBar);
     }
 }
