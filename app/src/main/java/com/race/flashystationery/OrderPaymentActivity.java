d03f1f2e0ed6703be73261832d0b5279117ae926
khoa <khoatdd20411c@st.uel.edu.vn>
2022-12-07 00:47:24 +0700

    Giỏ hàng

--- app/src/main/java/com/race/flashystationery/OrderPaymentActivity.java
+++ app/src/main/java/com/race/flashystationery/OrderPaymentActivity.java
@@ -1,10 +1,12 @@
 package com.race.flashystationery;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -36,6 +38,7 @@ public class OrderPaymentActivity extends AppCompatActivity {
         binding = ActivityOrderPaymentBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         getSupportActionBar().setTitle("Kiểm tra");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         addEvents();
     }
 
@@ -73,6 +76,7 @@ public class OrderPaymentActivity extends AppCompatActivity {
                     setRadioButtonStyle(radBank);
                     setRadioButtonStyle(radCash);
 //                    GRadioGroup radPM = new GRadioGroup(radCash, radMomo, radZalo, radBank);
+                    radCash.setChecked(true);
                     btnConfirmPM.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
@@ -131,10 +135,10 @@ public class OrderPaymentActivity extends AppCompatActivity {
                     radStandard = bottomSheetShip.findViewById(R.id.rad_Standard);
                     radFast = bottomSheetShip.findViewById(R.id.rad_Fast);
 
-
                     setRadioButtonStyle(radFast);
                     setRadioButtonStyle(radStandard);
 //                    GRadioGroup radShip = new GRadioGroup(radStandard, radFast);
+                    radStandard.setChecked(true);
                     radStandard.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
@@ -169,8 +173,20 @@ public class OrderPaymentActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         loadItemData();
+        loadData();
         loadMethodData();
         super.onResume();
+    }
+
+    private void loadData() {
+        double preTotalPrice = 0, totalPrice = 0;
+        for (int position = 0; position < items.size(); position++){
+            preTotalPrice += items.get(position).getItemPrice();
+        }
+        totalPrice = preTotalPrice + 16000;
+        binding.txtPreTotalPrice.setText(preTotalPrice + "đ");
+        binding.txtTotalPrice.setText( totalPrice + "đ");
+        binding.includeOrderFooter.txtFinalTotalPrice.setText(totalPrice + " đ");
     }
 
     private void loadMethodData() {
@@ -186,7 +202,7 @@ public class OrderPaymentActivity extends AppCompatActivity {
         binding.gvPaymentMethod.setItemChecked(0, true);
     }
 
-    private void loadItemData() {
+    public void loadItemData() {
         items = new ArrayList<>();
         items.add(new Item(R.drawable.notebook, "1", "Sổ Tay Ghi Chép giấy kraft Nâu Có Dòng Kẻ", "Phân loại: 100 trang, Mẫu: 05", "SALE: kết thúc 31 thg 12 23:59:59", 48000));
         items.add(new Item(R.drawable.notebook4, "2", "Sổ Tay Ghi Chép giấy kraft Nâu Có Dòng Kẻ", "Phân loại: 100 trang, Mẫu: 05", null, 96000));
@@ -194,5 +210,15 @@ public class OrderPaymentActivity extends AppCompatActivity {
         orderAdapter = new ItemOrderAdapter(OrderPaymentActivity.this, R.layout.order_payment_item_list, items);
         binding.lvOrderPaymentItem.setAdapter(orderAdapter);
         binding.lvOrderPaymentItem.setExpanded(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
