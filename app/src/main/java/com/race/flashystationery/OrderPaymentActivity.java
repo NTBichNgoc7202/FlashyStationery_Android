2250e3197f332ffd9c10cd5325e43ab80fc38d43
khoa <khoatdd20411c@st.uel.edu.vn>
2022-12-06 19:06:33 +0700

    Giỏ hàng

--- app/src/main/java/com/race/flashystationery/OrderPaymentActivity.java
+++ app/src/main/java/com/race/flashystationery/OrderPaymentActivity.java
@@ -2,9 +2,12 @@ package com.race.flashystationery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.race.adapters.ItemAdapter;
+import com.race.adapters.ItemOrderAdapter;
 import com.race.flashystationery.databinding.ActivityOrderPaymentBinding;
 import com.race.models.Item;
 
@@ -13,7 +16,7 @@ import java.util.ArrayList;
 public class OrderPaymentActivity extends AppCompatActivity {
 
     ActivityOrderPaymentBinding binding;
-    ItemAdapter itemAdapter;
+    ItemOrderAdapter orderAdapter;
     ArrayList<Item> items;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,17 @@ public class OrderPaymentActivity extends AppCompatActivity {
         binding = ActivityOrderPaymentBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         getSupportActionBar().setTitle("Kiểm tra");
+        addEvents();
+    }
+
+    private void addEvents() {
+        binding.llAddressDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(OrderPaymentActivity.this, AddressListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -31,6 +45,11 @@ public class OrderPaymentActivity extends AppCompatActivity {
     }
 
     private void loadItemData() {
-        
+        items = new ArrayList<>();
+        items.add(new Item(R.drawable.notebook, "1", "Sổ Tay Ghi Chép giấy kraft Nâu Có Dòng Kẻ", "Phân loại: 100 trang, Mẫu: 05", "SALE: kết thúc 31 thg 12 23:59:59", 48000));
+        items.add(new Item(R.drawable.notebook4, "2", "Sổ Tay Ghi Chép giấy kraft Nâu Có Dòng Kẻ", "Phân loại: 100 trang, Mẫu: 05", null, 96000));
+
+        orderAdapter =  new ItemOrderAdapter(OrderPaymentActivity.this, R.layout.order_payment_item_list, items);
+        binding.lvOrderPaymentItem.setAdapter(orderAdapter);
     }
 }
