7544c561e562dbc29de85d4eea236a7400995e61
VoThiNgocTrinh20411c <trinhvtn20411c@st.uel.edu.vn>
2022-12-08 12:58:11 +0700

    update code

--- app/src/main/java/com/race/flashystationery/LoginRegisterActivity.java
+++ app/src/main/java/com/race/flashystationery/LoginRegisterActivity.java
@@ -6,43 +6,77 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import com.race.flashystationery.databinding.ActivityLoginRegisterBinding;
 
 public class LoginRegisterActivity extends AppCompatActivity {
 
-    ActivityLoginRegisterBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login_register);
+        //Set the title
+        getSupportActionBar().setTitle("Firebase App");
 
-        binding = ActivityLoginRegisterBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
-
-        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+        //Open Login Activity
+        Button btnSignIn = findViewById(R.id.btn_Login);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(LoginRegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        //Open Register Activity
+        Button btnSiggUp = findViewById(R.id.btn_Register);
+        btnSiggUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginRegisterActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+        //Close LoginRegister Page and go to Home Page
+        TextView txtPass = findViewById(R.id.txt_Pass);
+        txtPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginRegisterActivity.this, MainActivity.class);
+                startActivity(intent);
             }
         });
 
-        binding.btnRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        binding.txtPass.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(LoginRegisterActivity.this,MainActivity.class));
-            }
-        });
+//
+//
+//        binding = ActivityLoginRegisterBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//
+//        ActionBar actionBar = getSupportActionBar();
+//        actionBar.hide();
+//
+//        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(LoginRegisterActivity.this, LoginActivity.class));
+//
+//            }
+//        });
+//
+//        binding.btnRegister.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(LoginRegisterActivity.this, RegisterActivity.class));
+//
+//            }
+//        });
+//
+//        binding.txtPass.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(LoginRegisterActivity.this,MainActivity.class));
+//            }
+//        });
     }
 }
